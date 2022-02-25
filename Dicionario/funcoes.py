e1dc8a0abfb16cf61647d0f9c644dbf00412258c
luk3mn <lucasrenan2015@gmail.com>
2022-02-25 19:07:21 -0300

    resolução dos exercicios da aula

--- Dicionario/funcoes.py
+++ Dicionario/funcoes.py
@@ -1,9 +1,9 @@
 def perguntar():
-    return input("O que deseja realizar?\n" +
+    return input("O que deseja realizar? [0 - Encerra]\n" +
                  "<I> - Para Inserir um usuário\n" +
                  "<P> - Para Pesquisar um usuário\n" +
                  "<E> - Para Excluir um usuário\n" +
-                 "<L> - Para Listar um usuário: ").upper()
+                 "<L> - Para Listar um usuário: \n").upper()
 
 
 def inserir(dicionario):
@@ -11,3 +11,33 @@ def inserir(dicionario):
     dicionario[chave] = [input("Digite o nome: ").upper(),
                          input("Digite a última data de acesso: "),
                          input("Qual a última estação acessada: ").upper()]
+
+
+def pesquisar(dicionario):
+    login = input("Informe o login: ").upper()
+    lista = dicionario.get(login);
+    if lista:
+        # print(dicionario.get(login))
+        print("Nome...............: " + lista[0])
+        print("Último acesso......: " + lista[1])
+        print("Última estação.....: " + lista[2])
+    else:
+        print("\"Login\" não existe!")
+
+
+def excluir(dicionario):
+    login = input("Informe o login: ").upper()
+    confirme = input("Tem certeza disso? [S/N]").upper()
+    if confirme == "S":
+        if dicionario.get(login):
+            del dicionario[login]
+            print("O usuário foi deletado!")
+        else:
+            print("Login não encontrado!")
+
+
+def listar(dicionario):
+    for chave, valor in dicionario.items():
+        print("Objeto..............")
+        print("Login:", chave)
+        print("Dados: ", valor)
