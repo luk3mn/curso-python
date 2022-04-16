11bd6490a96de1e91fbc32f9feef88fd4400877f
luk3mn <lucasrenan2015@gmail.com>
2022-04-16 11:14:24 -0300

    trabalhando na busca dos produtos

--- 0_praticas/Manipulacao_Arquivos/Inventory/App.py
+++ 0_praticas/Manipulacao_Arquivos/Inventory/App.py
@@ -1,33 +1,21 @@
 from Module import *
 
-# dicionary = {
-#     "120":["Refrigerante", 5.70, "Bebidas", "14/04/2022", 10, 25],
-#     "121":["Leite", 6.40, "Laticínios", "14/04/2022", 15, 5],
-#     "122":["Bolo", 3.35, "Doces", "14/04/2022", 4, 54]
-# }
-
 option = options()
 while option != 0:
     if option == 1:
-        register()
+        register("db.json")
     elif option == 2:
-        print("opção 2")
+        code = input("Informe a categoria:")
+        show("db.json", cat_code=code)
     elif option == 3:
         code = input("Informe o código do produto: ")
-        show("db.json", code)
+        show("db.json", code=code)
     elif option == 4:
-        print("opção 4")
+        code = input("Informe o código do produto: ")
+        update("db.json", code)
     elif option == 5:
-        print("opção 5")
+        code = input("Informe o código do produto: ")
+        delete("db.json", code)
     else:
         print("Opção inválida")
     option = options()
-
-
-# with open("db.json", "w") as json_file:
-#   json.dump(dicionary, json_file)
-
-# with open("db.json", "r") as json_file:
-#   dicionary = json.load(json_file)
-#   for chave, valor in dicionary.items():
-#     print(chave + " | " + str(valor))
--- 0_praticas/Manipulacao_Arquivos/Inventory/Module.py
+++ 0_praticas/Manipulacao_Arquivos/Inventory/Module.py
@@ -1,17 +1,19 @@
 import json
 import os
+from datetime import date
 
+# função para perguntar as opções ao usuário
 def options():
-  options = int(input("[1] Cadastrar produto\n"
-                      "[2] Listar produtos por categorias\n"
-                      "[3] Pesquisar produto\n"
-                      "[4] Atualizar produto\n"
-                      "[5] Remover produto\n"
-                      "[0] Encerrar\n"
-                      "Escolha uma opção: "))
-  return options
+  return int(input("[1] Cadastrar produto\n"
+                   "[2] Listar produtos por categorias\n"
+                   "[3] Pesquisar produto\n"
+                   "[4] Atualizar produto\n"
+                   "[5] Remover produto\n"
+                   "[0] Encerrar\n"
+                   "Escolha uma opção: "))
 
 
+# carrega o arquivo para a leitura
 def read_file(file):
   if os.path.exists(file): # se o aquivo ja existe -> adiciona seus dados no dicionario
     with open(file, "r") as json_file:
@@ -20,13 +22,54 @@ def read_file(file):
     dicionary = {}
   return dicionary
 
+# cria um novo arquivo passando os dados do dicionario para ele
+def save_file(dicionary, file):
+  with open(file, "w") as json_file:
+    json.dump(dicionary, json_file)
 
-def register():
-  print("Cadastar produto")
+# registra os dados do produto em um dicionario de dados
+def register(file):
+  # carrega os dados do arquivo para o dicionario de dados
+  dicionary = read_file(file)
+  sysDate = date.today() # pega a hora atual
+  [description, prod_code, cat_code, price, category, quantity] = [
+    input("Informe os dados do produto\nDescrição do produto:"),
+    input("Código do produto: "),
+    input("Código da categoria: "),
+    input("Preço: "),
+    input("Categoria: "),
+    input("Quantidade: ")
+  ]
 
-# MELHORAR A EXIBIÇÃO
-def show(file, code):
+  # registra dos dados de entrada no dicionario de dados
+  dicionary[prod_code]=[description, cat_code, float(price), category, str(sysDate), int(quantity)]
+  # salva os dados do dicionario do arquivo json
+  save_file(dicionary, file)
+
+
+def show(file, code=None, cat_code=None):
   dicionary = read_file(file) # será passado o nome e endereço do arquivo  
+  for key, values in dicionary.items():
+    if code == key:
+      print("Cód. Produto...: ", key)
+      print("Descrição......: ", values[0])
+      print("Cód. Categoria.: ", values[1])
+      print("Preço..........: ", "R$",values[2])
+      print("Categoria......: ", values[3])
+      print("Cadastrado em..: ", values[4])
+      print("Quantidade.....: ", values[5])
+    if cat_code == values[1]:
+      print("Produto: {}\nCategoria: {}\n".format(values[0], values[3]))
+
+def update(file, code):
+  dicionary = read_file(file)
+  for key, values in dicionary.items():
+    if code == key:
+      print(values)
+
+
+def delete(file, code):
+  dicionary = read_file(file)
   for key, values in dicionary.items():
     if code == key:
       print(values)
