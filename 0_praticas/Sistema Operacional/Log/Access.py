9aeea4b0ed4e2b08441897d975b558b102375eda
luk3mn <lucasrenan2015@gmail.com>
2022-04-19 20:26:02 -0300

    exerc: ajustando o mecanismo de add de dados de log no arquivo json

--- 0_praticas/Sistema Operacional/Log/Access.py
+++ 0_praticas/Sistema Operacional/Log/Access.py
@@ -1,9 +1,11 @@
-import getpass
-import os
-import json
 import platform
+import getpass
+import random
+import json
+import os
 from datetime import datetime
 
+# Mecanismo de login
 def login(dicionario):
   
   [usuario, senha] = [input("Informe seu login de acesso\nUsuário: "),
@@ -12,7 +14,7 @@ def login(dicionario):
   for chave, valor in dicionario.items():
     if senha == chave and usuario == valor[0]:
       # print("Usuário: ", valor[0], "Senha: ", chave)
-      return True
+      return True, usuario
 
 
 # carrega todos os dados de log exitente no arquivo de log
@@ -25,26 +27,31 @@ def carregar_dados_log(arquivo):
   return dicionario
 
 
+# 
+def grava_dados_log(dicionario, arquivo):
+  with open(arquivo, "w") as arq_log:
+      json.dump(dicionario, arq_log)
 
-def access_log(access, arquivo):
+
+# Mecanismo que add no dic
+def access_log(access, usuario, arquivo):
   if access:
     dicionario = carregar_dados_log(arquivo)
     
-    [nome_maq, so, versao, usuario, data_hora] = [
+    [nome_maq, so, versao, usuario_so, data_hora] = [
       platform.node(), platform.system(),
       platform.version(), getpass.getuser(),
       datetime.now()
     ]
+    
+    # gera um id aleatório para cada acesso
+    _id = ''
+    for x in range(0,3):
+      _id = _id + str(random.randrange(10,99))
 
     # TAREFA: ATRIBUIR OUTRA CHAVE {EXP: USUÁRIO+VALOR_ALEATÓRIO}
-    dicionario[usuario] = [nome_maq, so, versao, str(data_hora)]
+    dicionario[usuario+_id] = [nome_maq, so, usuario_so, versao, str(data_hora)]
 
-    with open("sys_access.json", "w") as arq_log:
-      json.dump(dicionario, arq_log)
+    # chama a função que grava o log no json
+    grava_dados_log(dicionario, arquivo)
     print(dicionario)
-
-    # print("Nome do computador..................: ", nome_maq)
-    # print("Nome do sistema operacional.........: ", so)
-    # print("Versão do sistema Operacional.......: ", versao)
-    # print("Nome de usuáiro.....................: ", usuario)
-    # print("Data/Hora do Acesso.................: ", data_hora)
--- 0_praticas/Sistema Operacional/Log/AccessLog.py
+++ 0_praticas/Sistema Operacional/Log/AccessLog.py
@@ -1,14 +1,12 @@
-import json
 from Access import *
-import platform
 from datetime import datetime
 
 usuarios = {"1998": ["lukemn", "19-04-2022"], "2030": ["renl", "18-04-2022"]}
 dados_log = {}
 
-logado = login(usuarios)
+logado, usuario = login(usuarios)
 if logado == True:
   print("Logado!")
-  access_log(logado, arquivo="sys_access.json")
+  access_log(logado, usuario, arquivo="sys_access.json")
 else:
   print("Incorreto")
